6653f1de94384ef14fc3c0787e2e39ecfb1bc0a5
Krzysztof <Krzysztof.Lema@fieldcode.com>
2021-03-10 16:43:07 +0100

    Added initial project files.

---
+++ GitHubSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by Krzysztof Lema on 10/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
