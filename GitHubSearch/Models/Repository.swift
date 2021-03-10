d4e9651be806c9e432715184bf4601ec5ee51a23
Krzysztof <Krzysztof.Lema@fieldcode.com>
2021-03-10 22:19:37 +0100

    added avatar url property

--- GitHubSearch/Models/Repository.swift
+++ GitHubSearch/Models/Repository.swift
@@ -11,8 +11,10 @@ struct GitRepository: Codable {
     var id: Int?
     var name: String?
     var url: URL?
-
+    var avatarURL: URL?
+    
     enum CodingKeys: String, CodingKey {
-       case url = "html_url"
+        case url = "html_url"
+        case avatarURL = "avatar_url"
     }
 }
